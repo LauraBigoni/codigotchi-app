170316c6480b94ba9ddf44f660b709f4c38e8e6c
Laura Bigoni <laurabigoni.92@gmail.com>
2022-06-15 14:44:09 +0200

    add on result function

--- script.js
+++ script.js
@@ -17,8 +17,34 @@ const recognition = new SpeechRecognition();
 // * Inizio ad ascoltare
 function onStartListening() {
   recognition.start();
+
+  // * Aggiungo la classe listening
   panelsData.classList.add('listening');
+}
+
+// * Funzione per controllare gli errori del recognition
+function onError(e) {
+  console.error(e.error);
+}
+
+// * Funzione per prendere il risultato del recognition
+function onResult(e) {
+  const testo = e.results[0][0].transcript;
+  // console.log(testo);
+
+  // * Cerco se la parola captata è tra i comandi
+  const action = commands.find(function (command) {
+    return testo.toLowerCase().includes(command);
+  });
+  console.log(action);
+
+  // * Rimuovo la classe listening
+  panelsData.classList.remove('listening');
 }
 
 // * Aggiungo gli event handler
 micButton.addEventListener('click', onStartListening);
+
+// * Gestisco gli event del recognition
+recognition.addEventListener('result', onResult);
+recognition.addEventListener('error', onError);
