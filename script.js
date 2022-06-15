10ba269b84742bcbcbea226123a9b2212ffebc36
Laura Bigoni <laurabigoni.92@gmail.com>
2022-06-15 14:34:51 +0200

    add id to btn and take all dom elements in js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("JS OK");
+
+// * Interfaccia di controllo del riconoscimento vocale
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+// * Prendo tutti gli elementi dal DOM
+const micButton = document.getElementById('mic');
+const screen = document.getElementById('screen');
+const panelsData = document.getElementById('panels-data');
+
+// * Salvo i possibili comandi in una variabile
+const commands = ["mangia", "balla", "dormi"];
+
+// * Inizializzazione comando vocale
